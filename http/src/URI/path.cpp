96d618032ff42c47efec1b447316fa6295047229
cteoh <teohcheeming@gmail.com>
2025-01-30 12:02:03 +0800

    docs(http): add comments to document/explain functions/concepts

--- http/src/URI/path.cpp
+++ http/src/URI/path.cpp
@@ -6,7 +6,7 @@
 /*   By: cteoh <cteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 22:05:10 by cteoh             #+#    #+#             */
-/*   Updated: 2025/01/25 21:41:46 by cteoh            ###   ########.fr       */
+/*   Updated: 2025/01/30 11:54:14 by cteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,23 @@
 #include "terminalValues.hpp"
 #include "path.hpp"
 
-bool	isPrintableCharacter(const std::string &line, std::size_t &index) {
-	static const std::string	values = ":@";
+/********/
+/* Path */
+/********/
+//	Serves to identify a resource within the scope of the URI's scheme and
+//	authority. It generally corresponds to the acutal file path in the web
+//	server's root directory.
 
-	if (isUnreservedCharacter(line[index]) == true)
-		return (true);
-	if (isPercentEncoded(line, index) == true) {
-		index += 2;
-		return (true);
+//	
+bool	isPathAbEmpty(const std::string &line) {
+	std::stringstream	stream(line);
+	std::string			str;
+
+	std::getline(stream, str, '/');
+	while (std::getline(stream, str, '/')) {
+		if (isSegment(str) == false)
+			return (false);	
 	}
-	if (isSubDelimiter(line[index]) == true)
-		return (true);
-	if (values.find(line[index]) != std::string::npos)
-		return (true);
-	return (false);
-}
-
-bool	isSegment(const std::string &line) {
-	for (std::size_t i = 0; i < line.length(); i++) {
-		if (isPrintableCharacter(line, i) == true)
-			continue ;
-		return (false);
-	}
-	return (true);
-}
-
-bool	isSegmentNZ(const std::string &line) {
-	if (line.length() == 0)
-		return (false);
-	if (isSegment(line) == false)
-		return (false);
 	return (true);
 }
 
@@ -55,6 +42,20 @@ bool	isPathAbsolute(const std::string &line) {
 
 	std::string	str = line.substr(1);
 	if (isPathRootless(str) == false)
+		return (false);
+	return (true);
+}
+
+bool	isPathNoScheme(const std::string &line) {
+	std::stringstream	stream(line);
+	std::string			str;
+
+	std::getline(stream, str, '/');
+	if (isSegmentNZNC(str) == false)
+		return (false);
+	std::getline(stream, str);
+	str = '/' + str;
+	if (isPathAbEmpty(str) == false)
 		return (false);
 	return (true);
 }
@@ -79,15 +80,29 @@ bool	isPathEmpty(const std::string &line) {
 	return (true);
 }
 
-bool	isPathAbEmpty(const std::string &line) {
-	std::stringstream	stream(line);
-	std::string			str;
+bool	isAbsolutePath(const std::string &line) {
+	if (line.length() == 0)
+		return (false);
 
-	std::getline(stream, str, '/');
-	while (std::getline(stream, str, '/')) {
-		if (isSegment(str) == false)
-			return (false);	
+	if (isPathAbEmpty(line) == false)
+		return (false);
+	return (true);
+}
+
+bool	isSegment(const std::string &line) {
+	for (std::size_t i = 0; i < line.length(); i++) {
+		if (isPrintableCharacter(line, i) == true)
+			continue ;
+		return (false);
 	}
+	return (true);
+}
+
+bool	isSegmentNZ(const std::string &line) {
+	if (line.length() == 0)
+		return (false);
+	if (isSegment(line) == false)
+		return (false);
 	return (true);
 }
 
@@ -111,16 +126,18 @@ bool	isSegmentNZNC(const std::string &line) {
 	return (true);
 }
 
-bool	isPathNoScheme(const std::string &line) {
-	std::stringstream	stream(line);
-	std::string			str;
+bool	isPrintableCharacter(const std::string &line, std::size_t &index) {
+	static const std::string	values = ":@";
 
-	std::getline(stream, str, '/');
-	if (isSegmentNZNC(str) == false)
-		return (false);
-	std::getline(stream, str);
-	str = '/' + str;
-	if (isPathAbEmpty(str) == false)
-		return (false);
-	return (true);
+	if (isUnreservedCharacter(line[index]) == true)
+		return (true);
+	if (isPercentEncoded(line, index) == true) {
+		index += 2;
+		return (true);
+	}
+	if (isSubDelimiter(line[index]) == true)
+		return (true);
+	if (values.find(line[index]) != std::string::npos)
+		return (true);
+	return (false);
 }

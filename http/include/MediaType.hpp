98697d9b4503ecf35ba33834e43c7b6afd6cb1bb
Chee ming Teoh <cteoh@u82z06s02.42kl.edu.my>
2025-02-06 22:34:21 +0800

    docs(http): add documentation for MediaType class

--- http/include/MediaType.hpp
+++ http/include/MediaType.hpp
@@ -6,7 +6,7 @@
 /*   By: cteoh <cteoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 19:05:34 by cteoh             #+#    #+#             */
-/*   Updated: 2025/02/05 03:41:04 by cteoh            ###   ########.fr       */
+/*   Updated: 2025/02/06 17:08:44 by cteoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 # include <map>
 # include "Optional.hpp"
 
+/***************************************/
+/* File extension to MIME type mapping */
+/***************************************/
+//	At the start of the program, constructs a map of file extensions to their
+//	respective MIME types. The map is based on specifications within a file
+//	(defaulted to 'mime.types').
+//
+//	This map would be used in file retrieval operations such as GET requests to
+//	construct the 'Content-Type' header.
 class MediaType {
 		MediaType(void);
 		MediaType(const MediaType &obj);

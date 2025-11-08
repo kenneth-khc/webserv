c06b08ea58a379b789f5dc02d1523c563d8d7101
Kenneth Cheong <kennethkhc29@gmail.com>
2025-11-08 21:08:41 +0800

    feat: abstract class ConfigError to be inherited and have .format capabilities

---
+++ include/config/errors/ConfigError.hpp
@@ -0,0 +1,26 @@
+#ifndef CONFIG_ERROR_HPP
+#define CONFIG_ERROR_HPP
+
+#include "String.hpp"
+#include <stdexcept>
+
+/* These are custom exceptions thrown by the Parser, to indicate things
+ * going wrong when parsing a config file. It provides the ability to display
+ * the error to the user, allowing them to correct the config file if they
+ * so desire. */
+
+class	ConfigError : public std::runtime_error
+{
+public:
+	ConfigError();
+	ConfigError(const String& filename);
+	~ConfigError() throw();
+
+	virtual const char*	what() const throw() = 0;
+	virtual String		format() const = 0;
+
+protected:
+	const String	filename;
+};
+
+#endif
